3ce150dbe9fff4e60ba793bfdf74bdf2b8bf7946
Vic <viccjuan@gmail.com>
2024-04-16 14:51:12 -0500

    PlayerLayer

--- Assets/_Scripts/Enemy/Enemy.cs
+++ Assets/_Scripts/Enemy/Enemy.cs
@@ -6,6 +6,7 @@ public class Enemy : MonoBehaviour
 {
     public Rigidbody2D enemyRB;
     public GameObject player;
+    public LayerMask playerLayer;
 
     public float health, speed, damage;
 
@@ -13,11 +14,12 @@ public class Enemy : MonoBehaviour
 
     public void DetectPlayer(float followDistance, GameObject player)
     {
+        // Just to see the ray
         Vector2 playerDirection = player.transform.position - transform.position;
         Ray2D rayToSee = new Ray2D(transform.position, playerDirection);
         Debug.DrawRay(rayToSee.origin, rayToSee.direction * followDistance, Color.green);
         
-        RaycastHit2D hit = Physics2D.Raycast(rayToSee.origin, playerDirection, followDistance);
+        RaycastHit2D hit = Physics2D.Raycast(rayToSee.origin, playerDirection, followDistance, playerLayer);
         if (hit.collider!=null)
         {
             Debug.Log("Collider "+hit.collider.name+" detected");
