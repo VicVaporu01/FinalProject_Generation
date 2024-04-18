d61fe28289edaea9d4365fdc24b20b26523bb788
flejrat <santid99@gmail.com>
2024-04-18 12:08:36 -0500

    commit 18/04
    Commit de la implementacion de las animaciones de movimiento al movimiento

--- Assets/_Scripts/Player/PlayerMovement.cs
+++ Assets/_Scripts/Player/PlayerMovement.cs
@@ -8,21 +8,41 @@ public class PlayerMovement : MonoBehaviour
     [Header("Player Input")]
     [SerializeField] private PlayerInput playerInput;
     private InputAction moveAction;
+    private InputAction attack;
+    
 
     [Header("Movement")]
     [SerializeField] private Rigidbody2D rb2D;
     [SerializeField] private float movementSpeed;
     [SerializeField] private Vector2 moveDirection;
 
+    Animator anims;
+
     private void Awake()
     {
         moveAction = playerInput.actions["Move"];
+        attack = playerInput.actions["Attack"];
+        anims = GetComponent<Animator>();
     }
 
     private void Update()
     {
         moveDirection = moveAction.ReadValue<Vector2>();
 
+        anims.SetFloat("MovementX", moveDirection.x);
+        anims.SetFloat("MovementY", moveDirection.y);
+        if (moveDirection.x != 0 || moveDirection.y != 0)
+        {
+            anims.SetFloat("LastMovX", moveDirection.x);
+            anims.SetFloat("LastMovY", moveDirection.y);
+        }
         rb2D.velocity = moveDirection * movementSpeed;
+        if (attack.WasPressedThisFrame() && attack.IsPressed())
+        {
+            Debug.Log("Precionado");
+            anims.SetTrigger("attack");
+        }
+        
     }
+
 }
