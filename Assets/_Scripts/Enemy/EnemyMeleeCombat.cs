9f34a348c9347569d6f2f735f7dc498c965287a4
Vic <viccjuan@gmail.com>
2024-04-21 22:08:24 -0500

    feat(enemy): Mage Teleport
    The mage teleport was made.

--- Assets/_Scripts/Enemy/EnemyMeleeCombat.cs
+++ Assets/_Scripts/Enemy/EnemyMeleeCombat.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MeleeCombat : MonoBehaviour
+public class EnemyMeleeCombat : MonoBehaviour
 {
     [SerializeField] private Transform attackController;
     [SerializeField] private float attackRatio;
--- Assets/_Scripts/Enemy/Mage.cs
+++ Assets/_Scripts/Enemy/Mage.cs
@@ -1,9 +1,12 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class Mage : Enemy
 {
+    [SerializeField] private float minDistanceValue, tpCooldown = 3.0f;
     private void Start()
     {
         SetEnemyRB(GetComponent<Rigidbody2D>());
@@ -15,4 +18,36 @@ public class Mage : Enemy
         base.DetectPlayer(followDistance, GetPlayer());
         base.FollowPlayer(GetPlayer());
     }
+
+    private void FixedUpdate()
+    {
+        CalculateApproach(minDistanceValue);
+        if (health<= 2.5f && !canApproach && tpCooldown<=0)
+        {
+            Debug.Log("Teleporting");
+            TeleportAway(minDistanceValue);
+            tpCooldown = 3.0f;
+        }
+        else
+        {
+            tpCooldown -= Time.deltaTime;
+        }
+        
+        // This is to make the enemy look at its walking direction
+        if (GetEnemyRB().velocity != Vector2.zero)
+        {
+            Vector3 lookDirection = new Vector3(GetEnemyRB().velocity.x, GetEnemyRB().velocity.y, 0);
+            transform.up = lookDirection;
+        }
+        
+        CalculateApproach(minDistanceValue);
+    }
+
+    private void TeleportAway(float distance)
+    { 
+        transform.position = 
+            new Vector3(transform.position.x + Random.Range(-distance, distance), 
+                transform.position.y + Random.Range(-distance, distance), 0);
+    }
+    
 }
