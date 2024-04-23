6a8c3a02bc18237b9257bd4926c14e570fe0fc94
Vic <viccjuan@gmail.com>
2024-04-23 14:58:29 -0500

    feat(spawns): Enemies spawn
    The enemy's spawn was created with the difficulty option.

--- Assets/_Scripts/Enemy/SpawnManager.cs
+++ Assets/_Scripts/Enemy/SpawnManager.cs
@@ -7,11 +7,34 @@ public class SpawnManager : MonoBehaviour
     [SerializeField] private GameObject[] enemies;
     [SerializeField] private List<Transform> spawnPoints;
     
-    [SerializeField] private float spawnRate = 2.0f;
+    [SerializeField] private Difficulty currentDifficulty;
+    [SerializeField] private float spawnRate;
     
     private void Start()
     {
+        SetSpawnRate(currentDifficulty);
         InvokeRepeating("SpawnEnemy", 1.0f, spawnRate);
+    }
+    
+    public void SetDifficulty(Difficulty difficulty)
+    {
+        currentDifficulty = difficulty;
+    }
+    
+    private void SetSpawnRate(Difficulty difficulty)
+    {
+        switch (difficulty)
+        {
+            case Difficulty.Easy:
+                spawnRate = 3.0f;
+                break;
+            case Difficulty.Medium:
+                spawnRate = 2.0f;
+                break;
+            case Difficulty.Hard:
+                spawnRate = 1.0f;
+                break;
+        }
     }
 
     private void SpawnEnemy()
@@ -19,7 +42,15 @@ public class SpawnManager : MonoBehaviour
         int randomEnemy = Random.Range(0, enemies.Length);
         int randomPosition = Random.Range(0, spawnPoints.Count);
 
+        Debug.Log("Spawn Rate: "+spawnRate);
         Instantiate(enemies[randomEnemy], spawnPoints[randomPosition].position, Quaternion.identity);
     }
     
+}
+
+public enum Difficulty
+{
+    Easy,
+    Medium,
+    Hard
 }
